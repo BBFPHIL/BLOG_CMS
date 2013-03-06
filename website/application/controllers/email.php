de1b94090a120c29ee276608d999aa4f01807e1d
Phillip Walker <pwalker@fordham.edu>
2013-03-05 19:06:44 -0500

    Email controller fixed

--- website/application/controllers/email.php
+++ website/application/controllers/email.php
@@ -6,38 +6,43 @@
 */
 
 class Email extends CI_Controller{
-	
-	//Constructor
-	function __constructor(argument){
+ 	
+/* WHY WILL THIS MAKE THE PROGRAM STOP WORKING???*/
+
+ 	//Constructor
+	function __construct(){
 		
-		parent::Controller();
+		parent::__construct();
 		
 	}
-	
+
 
 	function index(){
+	
 		$config = array(
 			'protocol' => 'smtp',
 			'smtp_host' => 'ssl://smtp.googlemail.com',
 			'smtp_port' => 465,
 			'smtp_user' => 'wunderwalk24@gmail.com',
-			'smtp_pass' => 'wunder24!'
+			'smtp_pass' => 'wunder24!',
 		);
 		
 		$this->load->library('email', $config);
-		$this->email->set_newline("\r]\n");
+		$this->email->set_newline("\r\n");
 		
-		$this->email->from('pwalker@fordham.edu', 'Phil Walker');
+		$this->email->from('wunderwalk24@gmail.com', 'Phil Walker');
 		$this->email->to('pwalker@fordham.edu');
 		$this->email->subject('Hey phil, your on your way to becoming a PHP developer!');
 		$this->email->message('Program 3 hours every day and you will become the best!');
-		
+
 		if($this->email->send()){
-			echo('our email was sent.');
+			echo 'our email was sent.';
 		}else{
 			show_error($this->email->print_debugger());
 		}
 	}
 	
+	
 }
 
+?>
